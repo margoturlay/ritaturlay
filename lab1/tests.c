2f2ccd22d9ea33709f5360b915328954ee34968c
margoturlay <80766559+margoturlay@users.noreply.github.com>
2021-03-21 00:49:29 +0300

    Update tests.c

--- lab1/tests.c
+++ lab1/tests.c
@@ -5,6 +5,9 @@
 void test()
 {
     assert(getsize(1) == 1);
+    assert(getsize(3) == 2);
+    assert(getsize(11) == 4);
+    assert(getsize(15) == 4);
     printf("All tests are passed.\n");
 }
 #undef main
